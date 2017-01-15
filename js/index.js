abff4263cf9ccd700fdeae1fbbba90dd8e1d1202
Steven <sir.fon1103@gmail.com>
2017-01-15 06:37:34 -0600

    Naive Attempt

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
 (function() {
-    var Painter = function(c)
-    {
-    	var canvas = c;
-		var id = myContext.createImageData(1,1);
-		var d  = id.data;
+    var pileSize = 200;
+    var maxPile = 4;
+    var data;
+    var Painter = function(c) {
+        this.canvas = c;
+        this.id = c.createImageData(1, 1);
+        this.d = this.id.data;
 
-		var drawPixel = function(x,y,r,g,b,a)
-		{
-			if(a === undefined)
-				a = 255;
-			d[0] = r;
-			d[1] = g;
-			d[2] = b;
-			d[3] = a;
-			canvas.putImageData(id,x,y);
-		}
+        this.drawPixel = function(x, y, r, g, b, a) {
+            if (a === undefined)
+                a = 255;
+            this.d[0] = r;
+            this.d[1] = g;
+            this.d[2] = b;
+            this.d[3] = a;
+            canvas.putImageData(this.id, x, y);
+        }
+    }
+
+    var create2dArray = function(length, depth) {
+        var arr = new Array(length);
+        for (var i = 0; i < length; i++)
+            arr[i] = new Array(depth);
+        return arr;
     }
 
     var init = function() {
@@ -24,6 +32,7 @@
         var controlsCol = document.getElementById("controlsColumn");
         controlsCol.style.width = (document.body.clientWidth - c.width) + "px";
         canvas = c.getContext('2d');
+        data = create2dArray(c.width, c.height);
     }
 
     init();
@@ -38,4 +47,51 @@
     var getAdjacentCellValue = function(thisCell, maxValue) {
         return roundUp((thisCell - maxValue) / 4);
     }
+
+    var inBounds = function(x, y) {
+        return x > 0 && x < data.length && y > 0 && y < data[0].length;
+    }
+
+    var step = function() {
+        var newArr = create2dArray(data.length, data[0].length);
+        var reduced = true;
+        for (var i = 0; i < data.length; i++) {
+            for (var j = 0; j < data[0].length; j++) {
+                if (data[i][j] > maxPile) {
+                    newArr[i][j] = data[i][j] - 4;
+                    if (inBounds(i - 1, j)) {
+                        newArr[i - 1][j] = data[i - 1][j] + 1;
+                        if (newArr[i - 1][j] > maxPile) {
+                            reduced = false;
+                        }
+                    }
+                    if (inBounds(i, j - 1)) {
+                        newArr[i][j - 1] = data[i][j - 1] + 1;
+                        if (newArr[i][j - 1] > maxPile) {
+                            reduced = false;
+                        }
+                    }
+                    if (inBounds(i + 1, j)) {
+                        newArr[i + 1][j] = data[i + 1][j] + 1;
+                        if (newArr[i + 1][j] > maxPile) {
+                            reduced = false;
+                        }
+                    }
+                    if (inBounds(i, j + 1)) {
+                        newArr[i][j + 1] = data[i][j + 1] + 1;
+                        if (newArr[i][j + 1] > maxPile) {
+                            reduced = false;
+                        }
+                    }
+                } else {
+                    newArr[i][j] = data[i][j];
+                }
+            }
+        }
+        data = newArr;
+        return reduced;
+    }
+
+    while (!step()) {};
+    console.log(data);
 })();
