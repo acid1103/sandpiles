e17d2caf541c4008a88da4b12babda19625f6163
Steven <sir.fon1103@gmail.com>
2017-01-14 21:54:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 (function(){
-var c = document.getElementById("myCanvas");
-c.width = Math.min(c.parentElement.clientWidth, c.parentElement.clientHeight);
-c.height = c.width;
+  var c = document.getElementById("myCanvas");
+  c.width = Math.min(document.body.clientWidth, document.body.clientHeight);
+  c.height = c.width;
+  var controlsCol = document.getElementById("controlsColumn");
+  controlsCol.style.width = (document.body.clientWidth - c.width)+"px";
 })();
